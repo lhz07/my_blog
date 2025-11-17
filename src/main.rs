3e778677565799e8c02419633782f01029d56117
lhz <a2722764368@gmail.com>
2025-11-18 07:52:57 +0800

    add onion link

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
+use my_blog::start_blog;
 use std::{io, net::TcpListener};
 
-use my_blog::start_blog;
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    unsafe {
-        #[cfg(debug_assertions)]
-        std::env::set_var("RUST_LOG", "info");
-        #[cfg(not(debug_assertions))]
-        std::env::set_var("RUST_LOG", "warn");
+    if std::env::var("RUST_LOG").is_err() {
+        unsafe {
+            #[cfg(debug_assertions)]
+            std::env::set_var("RUST_LOG", "info");
+            #[cfg(not(debug_assertions))]
+            std::env::set_var("RUST_LOG", "warn");
+        }
     }
     env_logger::init();
     initialize_static_vars();
+    let path = "0.0.0.0:8000";
+    let listener = TcpListener::bind(path)?;
     #[cfg(debug_assertions)]
     {
         use tokio::net::UdpSocket;
         let socket = UdpSocket::bind("0.0.0.0:0").await?;
         socket.connect("192.168.1.1:2333").await?;
         let local_addr = socket.local_addr()?;
-        log::info!("Local IP: {}", local_addr.ip());
+        // link: \e]8;;<URL>\a<label>\e]8;;\a
+        // e: x1b, a: x07
+        // underline + blue: \x1b[4;34m<label>\x1b[0m
+        // 4: underline, 34: blue, 0: reset
+        log::info!(
+            "Local IP: \x1b]8;;http://{0}:{1}\x1b\\http://{0}:{1}\x1b]8;;\x1b\\",
+            local_addr.ip(),
+            listener.local_addr()?.port(),
+        );
     }
-    let path = "0.0.0.0:8000";
-    let listener = TcpListener::bind(path)?;
     start_blog(listener)?.await?;
     Ok(())
 }
